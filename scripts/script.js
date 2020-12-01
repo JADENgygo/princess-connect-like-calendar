8d132a02a876fea463f9f9f8178fa1c67308d1bd
JADEN <29948746+JADENgygo@users.noreply.github.com>
2020-12-01 09:16:16 +0900

    レイアウトを変更

--- scripts/script.js
+++ scripts/script.js
@@ -26,12 +26,13 @@ document.addEventListener('DOMContentLoaded', () => {
 				localStorage.setItem('memberName' + index, event.target.value);
 			},
 			moveFocus: function(event, index) {
+				event.preventDefault();
 				if (event.keyCode === 13) {
 					let i = index;
 					if (index === 28) {
 						i = -1;
 					}
-					document.getElementById('memberName' + (i + 1)).focus();
+					document.getElementById('member-name' + (i + 1)).focus();
 				}
 			},
 			incrementLike: function(index) {
@@ -58,30 +59,33 @@ document.addEventListener('DOMContentLoaded', () => {
 			<div class="uk-container uk-text-center">
 				<div class="uk-text-lead uk-text-center uk-margin-top">いいねカウンター</div>
 				<div class="uk-margin-top">プリコネRのクランメンバーのいいね管理ツール (データはブラウザに保存されます)</div>
-				<select class="uk-select uk-form-small uk-form-width-xsmall uk-margin-top" v-model="memberCount" v-on:change="saveMemberCount($event)">
-					<option v-for="i in 29">{{ i }}</option>
-				</select>
-				<button class="uk-button uk-button-danger uk-button-small uk-margin-top uk-margin-left" uk-toggle="target: #dialog">いいねのリセット</button>
-				<div id="dialog" uk-modal>
-					<div class="uk-modal-dialog uk-modal-body">
-						<div class="uk-margin-bottom">いいねをリセットしますか</div>
-						<div class="uk-text-right">
-							<button class="uk-button uk-button-default uk-button-small uk-modal-close">キャンセル</button>
-							<button class="uk-button uk-button-danger uk-button-small uk-modal-close uk-margin-left" v-on:click="resetLikes()">リセット</button>
+				<div style="display: inline-block; text-align: left">
+					<select class="uk-select uk-form-small uk-form-width-xsmall uk-margin-top" v-model="memberCount" v-on:change="saveMemberCount($event)">
+						<option v-for="i in 29">{{ i }}</option>
+					</select>
+					<button class="uk-button uk-button-danger uk-button-small uk-margin-top uk-margin-left" uk-toggle="target: #dialog">いいねのリセット</button>
+					<div id="dialog" uk-modal>
+						<div class="uk-modal-dialog uk-modal-body">
+							<div class="uk-margin-bottom">いいねをリセットしますか</div>
+							<div class="uk-text-right">
+								<button class="uk-button uk-button-default uk-button-small uk-modal-close">キャンセル</button>
+								<button class="uk-button uk-button-danger uk-button-small uk-modal-close uk-margin-left" v-on:click="resetLikes()">リセット</button>
+							</div>
 						</div>
 					</div>
+					<template v-for="i in 29">
+						<form class="uk-form-stacked uk-margin-small-top" v-if="i <= memberCount">
+							<label class="uk-form-label" v-bind:for="'member-name' + (i - 1)">{{ 'メンバー' + i }}</label>
+							<div class="uk-form-controls">
+								<input v-bind:id="'member-name' + (i - 1)" class="uk-input uk-form-small uk-form-width-small" type="text" v-on:input="saveMemberNames($event, i - 1)" v-on:keydown="moveFocus($event, i - 1)" v-model="memberNames[i - 1]" v-bind:tabindex="i">
+								<button type="button" class="uk-button uk-button-default uk-button-small" v-on:click="incrementLike(i - 1)">+</button>
+								<button type="button" class="uk-button uk-button-default uk-button-small" v-on:click="decrementLike(i - 1)">-</button>
+								<span id="like" class="uk-badge">{{ likes[i - 1] }}</span>
+							</div>
+						</form>
+					</template>
+					<div class="uk-margin-top uk-margin-bottom"><span class="uk-text-muted">サイト作成者: </span><a class="uk-text-muted" href="https://twitter.com/JADENgygo">@JADENgygo</a></div>
 				</div>
-				<template v-for="i in 29">
-					<div class="uk-margin-small-top" v-if="i <= memberCount">
-						<span v-if="i <= 9">{{ i }}</span>
-						<span v-else>{{ i }}</span>
-						<input v-bind:id="'memberName' + (i - 1)" class="uk-input uk-form-small uk-form-width-small" type="text" v-on:input="saveMemberNames($event, i - 1)" v-on:keydown="moveFocus($event, i - 1)" v-model="memberNames[i - 1]" v-bind:tabindex="i">
-						<button class="uk-button uk-button-default uk-button-small" v-on:click="incrementLike(i - 1)">+</button>
-						<button class="uk-button uk-button-default uk-button-small" v-on:click="decrementLike(i - 1)">-</button>
-						<span id="like" class="uk-badge">{{ likes[i - 1] }}</span>
-					</div>
-				</template>
-				<div class="uk-margin-top uk-margin-bottom"><span class="uk-text-muted">サイト作成者: </span><a class="uk-text-muted" href="https://twitter.com/JADENgygo">@JADENgygo</a></div>
 			</div>
 		`
 	};
